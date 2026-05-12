1e9655812b6e420122f8a3cf34796de1d31af152
FelipeKatao <cataofelipe@gmail.com>
2026-05-12 13:29:32 -0300

    Recontrução do site base

--- AnaliseDados/Js/index.js
+++ AnaliseDados/Js/index.js
@@ -1,85 +1,11 @@
-import { Oct8 } from "./Oct8/Oct.js"
-import { ApiData } from "./util/Api_data.js"
-import { Banner } from "./Components/banner.js"
-import { Header } from "./Components/Header.js"
-import { DataCard } from "./Components/DataCard.js"
-import { ProjetoFrame } from "./Components/ProjetoFrame.js"
-import { ProjectCard } from "./Components/ProjectCard.js"
-import { ProfilePage } from "./page/profile.js"
-import { Headertitle } from "./Components/HeaderTitles.js"
-import { Graphs } from "./Components/Graphs.js"
-import { Container } from "./Components/Containers.js"
-import { DashBoardPage } from "./page/Dashboard.js"
-import { Widgets } from "./Components/Widget.js"
-import { MenuDash } from "./Components/MenuDash.js"
-import { Menu } from "./Components/Menu.js"
-import { Modal } from "./Components/modal.js"
-import { Badge } from "./Components/Badges.js"
-import { Table } from "./Components/table.js"
-import ToolTip from "./Components/Tooltip.js"
-//https://www.youtube.com/watch?v=5638u_F9_hY
-Oct8.Factory.register("Badge",Badge)
-Oct8.Factory.register("Banner",Banner)
-Oct8.Factory.register("Header",Header)
-Oct8.Factory.register("DataCard",DataCard)
-Oct8.Factory.register("ProjetoF",ProjetoFrame)
-Oct8.Factory.register("ProjetoCard",ProjectCard)
-Oct8.Factory.register("HeaderTitle",Headertitle)
-Oct8.Factory.register("Graphs",Graphs)
-Oct8.Factory.register("div",Container)
-Oct8.Factory.register("widgets_",Widgets)
-Oct8.Factory.register("MenuDash",MenuDash)
-Oct8.Factory.register("MenuSite",Menu)
-Oct8.Factory.register("modal",Modal)
-Oct8.Factory.register("Table",Table)
-Oct8.Factory.register("TollTip",ToolTip)
-
-Oct8.Styled.register("WhiteTheme",'./css/white.css')
-Oct8.Styled.set("WhiteTheme")
-
-Oct8.Reaction.create("filtroProjeto","Nenhum projeto filtrado")
-
-Oct8.Route.register("home",()=>{
-    document.getElementById("page").innerHTML = ""
-    
-    Oct8.Factory.render("MenuSite","#page",{})
-    let page = new ProfilePage()    
-},"/")
-
-class Space extends Oct8.FactoryClass {
-    constructor(prop) {
-        super()
-        this.props = prop
-        let element = ""
-        for (let index = 0; index < this.props.espace; index++) {
-            element += "</br>"
-
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
         }
+    });
+});
 
-        this.FactoryObj = `
-            <div id='espace_top'>
-            ${element}
-            </div>
-        `
-    }
-}
-
-Oct8.Factory.register("espace", Space)
-
-
-async function loadProject(){
-    let Projetos = new ApiData()
-    let project = await Projetos.ReadProject()
-    let NamesProject = Object.keys(project)
-    NamesProject.forEach(el =>{
-        Oct8.Route.register("dashboard",()=>{
-            document.getElementById("page").innerHTML = ""
-            Oct8.Factory.render("MenuSite","#page",{})
-            let page = new DashBoardPage(el)
-        },"#"+el)
-    })
-    Oct8.Route.RunRoutes() 
-}
-loadProject()
-Oct8.Styled.InitCSS()
-
+document.querySelectorAll(".hidden").forEach((el)=>{
+    observer.observe(el);
+});
